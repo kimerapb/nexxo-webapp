a86b5b63ef28483ff972b5d6eba4712f765591ec
kimerapb <publiactiva@gmail.com>
2022-02-28 16:26:24 -0600

    v1.2

--- comandos.js
+++ comandos.js
@@ -1,28 +1,30 @@
 //video de referencias muy bueno
-https://www.youtube.com/watch?v=iT4UOkyI09k
+https: //www.youtube.com/watch?v=iT4UOkyI09k
+
+    token personal: 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
 
 //VERSION DE GIT ESTO ESTA EN LA RAMA V3
-git --version
+git--version
 
 //git init --  para crear el contenido que necesita git para trabajar
 gig init
 
 // git add. -- prepara los archivos para nuestro commit
-git add .
+git add.
 
 // git reset --  revierte el git add
-git reset . 
+git reset.
 
 //git commit -m  -- es como crear una veersión o tomar una fotografia
-git commit -m "TEXTO REFERENTE"
+git commit - m "TEXTO REFERENTE"
 
 // git checkout -- . -- restablece ultimo commit realizado recostruye
-git checkout -- .
+git checkout--.
 
 //git log para ver los comits
 git log
 // parra arreglas el nombre del ultimo commit
-git commit --amend
+git commit--amend
 
 // para escribir  -- i.     w = write q = salir ! = para realizarlo
 // para salir tecla_escape + :wq! para que lo cambie
@@ -31,17 +33,17 @@ git commit --amend
 git branch
 
 //CREAMOS UNA NUEVA RAMA EL NOMBRE VA SIN COMILLAS
-git checkout -b "rama-v3"
+git checkout - b "rama-v3"
 
 // para cambiarse de rama  "rama" es la rama a donde voy a ir
 git checkout rama
 
 //unir ramas por ejemplo estoy en la rama-v2 y le uno la rama-v3
 
-git merge rama-v3
+git merge rama - v3
 
 //borrar la rama "rama-v3" es el nombre de la rama que voy a borrar.
-git branch -d rama-v3 
+git branch - d rama - v3
 
 
 //git pull
@@ -51,19 +53,19 @@ git
 git push
 
 //sirve para subir la rama a github cuando no existe en github si ya existe solo con git push sube
-git push --set-upstream origin v3
+git push--set - upstream origin v3
 
 //si el archivo ya existe pero solo vamos a grabar cambios
 // a = agregar 0 add + m = mensaje
-git commit -am "agregado"
+git commit - am "agregado"
 
 
 //bajar proyecto de github
-git clone git@github.com:Intelego-Nexxo/nexxo-webapp.git
+git clone git @github.com: Intelego - Nexxo / nexxo - webapp.git
 
 git remote add origin "NOMBRE DEL REPOSITORIO SIN COMILLAS"
-git branch -M main
-git push -u origin main
+git branch - M main
+git push - u origin main
 
-git remote -vv
-git push -u origin
+git remote - vv
+git push - u origin
