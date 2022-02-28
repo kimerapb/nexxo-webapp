97f124b7367db24bb55303b95bfe70b602849c00
kimerapb <publiactiva@gmail.com>
2022-02-28 14:54:12 -0600

    v1.0

---
+++ comandos.js
@@ -0,0 +1,69 @@
+//video de referencias muy bueno
+https://www.youtube.com/watch?v=iT4UOkyI09k
+
+//VERSION DE GIT ESTO ESTA EN LA RAMA V3
+git --version
+
+//git init --  para crear el contenido que necesita git para trabajar
+gig init
+
+// git add. -- prepara los archivos para nuestro commit
+git add .
+
+// git reset --  revierte el git add
+git reset . 
+
+//git commit -m  -- es como crear una veersión o tomar una fotografia
+git commit -m "TEXTO REFERENTE"
+
+// git checkout -- . -- restablece ultimo commit realizado recostruye
+git checkout -- .
+
+//git log para ver los comits
+git log
+// parra arreglas el nombre del ultimo commit
+git commit --amend
+
+// para escribir  -- i.     w = write q = salir ! = para realizarlo
+// para salir tecla_escape + :wq! para que lo cambie
+
+//sirve para ver todas las ramas
+git branch
+
+//CREAMOS UNA NUEVA RAMA EL NOMBRE VA SIN COMILLAS
+git checkout -b "rama-v3"
+
+// para cambiarse de rama  "rama" es la rama a donde voy a ir
+git checkout rama
+
+//unir ramas por ejemplo estoy en la rama-v2 y le uno la rama-v3
+
+git merge rama-v3
+
+//borrar la rama "rama-v3" es el nombre de la rama que voy a borrar.
+git branch -d rama-v3 
+
+
+//git pull
+git pull
+git
+//git push sirve para subir a gitub
+git push
+
+//sirve para subir la rama a github cuando no existe en github si ya existe solo con git push sube
+git push --set-upstream origin v3
+
+//si el archivo ya existe pero solo vamos a grabar cambios
+// a = agregar 0 add + m = mensaje
+git commit -am "agregado"
+
+
+//bajar proyecto de github
+git clone git@github.com:Intelego-Nexxo/nexxo-webapp.git
+
+git remote add origin "NOMBRE DEL REPOSITORIO SIN COMILLAS"
+git branch -M main
+git push -u origin main
+
+git remote -vv
+git push -u origin
